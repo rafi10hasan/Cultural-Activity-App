9b38892695cea47befc819ce2a8c5600b63afcac
Rafi Hasan <82175684+rafi10hasan@users.noreply.github.com>
2022-09-29 17:41:33 +0600

    faq question added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Cultural from './components/Cultural/Cultural';
+import Faq from './components/Faq/Faq';
 import Header from './components/Header/Header';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
          <Header></Header>
         <Cultural></Cultural> 
+        <Faq></Faq>
     </div>
   );
 }
