fb1a967320a4f3ca80bbd72634a2fe4fb0bea087
Rafi Hasan <82175684+rafi10hasan@users.noreply.github.com>
2022-09-29 15:47:51 +0600

    right side content added

--- src/components/Cultural/Cultural.js
+++ src/components/Cultural/Cultural.js
@@ -25,7 +25,54 @@ const Cultural = () => {
             </div>
 
             <div className="cultural-right-container">
-                 <h1>right</h1>
+                 <h3>Md.Rafi Hasan</h3>
+                 <h4>Dhanmondi,shukrabad</h4>
+                 <div className='user'>
+                      <div>
+                          <h3>70KG</h3>
+                          <h4>Weight</h4>
+                      </div>
+
+                      <div>
+                          <h3>5.8</h3>
+                          <h4>Height</h4>
+                      </div>
+
+                      <div>
+                          <h3>24YRS</h3>
+                          <h4>Age</h4>
+                      </div>
+                 </div>
+                 <h4>Details:{culturals.length}</h4>
+
+                 <div>
+                    <h3>Add a Break:</h3>
+                    <div className='break-content'>
+                         <button>10s</button>
+                         <button>20s</button>
+                         <button>30s</button>
+                         <button>40s</button>
+                         <button>50s</button>
+                    </div>
+                 </div>
+
+                 <div>
+                     <h3>Exercise Details:</h3>
+                     <div className='exercise-details'>
+                        <h3>Exercise Time:</h3>
+                        <h3>0s</h3>
+                     </div>
+                      
+                     <div className='exercise-details'>
+                        <h3>Break Time:</h3>
+                        <h3>0s</h3>
+                     </div>
+
+                 </div>
+
+                 <div className='activity'>
+                    <button>Activity Completed</button>
+                 </div>
             </div>
         </div>
     );
